5d8ed7eb19eca55850ef580a8062ad2889cf7ca0
DIRECT0R-ctrl <DominantVortex@proton.me>
2025-12-30 21:18:37 +0100

    creating the database and roles table

---
+++ database/database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE athena;
+\c athena;
+
+-- =========================
+-- ROLES
+-- =========================
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
