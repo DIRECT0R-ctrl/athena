8256c965d23fb41d5028ecb19cb135e0ca906443
DIRECT0R-ctrl <DominantVortex@proton.me>
2025-12-30 21:26:03 +0100

    insert name into roles and create users tables

--- database/database.sql
+++ database/database.sql
@@ -8,3 +8,22 @@ CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
 );
+
+INSERT INTO roles (name) VALUES
+('admin'),
+('chef_projet'),
+('membre');
+
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL,
+
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
