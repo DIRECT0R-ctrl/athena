56eb22db38775204413ef7c4fde661b3c4376636
DIRECT0R-ctrl <DominantVortex@proton.me>
2026-01-02 21:32:08 +0100

    declare all constants (constants.php)

---
+++ config/constants.php
@@ -0,0 +1,20 @@
+<?php
+define('ROLE_ADMIN', 1);
+define('ROLE_CHEF_PROJET', 2);
+define('ROLE_MEMBRE', 3);
+
+
+define('STATUS_TODO', 1);
+define('STATUS_IN_PROGRESS', 2);
+define('STATUS_DONE', 3);
+
+
+define('PRIORITY_LOW', 1);
+define('PRIORITY_MEDIUM', 2);
+define('PRIORITY_HIGH', 3);
+
+
+define('APP_NAME', 'Athena');
+define('APP_VERSION', '1.0.0');
+define('BASE_URL', 'http://localhost/athena');
+?>
