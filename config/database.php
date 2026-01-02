110aac1c14b80da2e6b347c2ba8d6784fb7232e9
DIRECT0R-ctrl <DominantVortex@proton.me>
2026-01-02 16:34:01 +0100

    database (database.php)

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'port' => 5432, // PostgreSQL default port
+    'database' => 'athena',
+    'username' => 'postgres', // or your PostgreSQL username
+    'password' => '', // your PostgreSQL password
+    'charset' => 'utf8'
+];
