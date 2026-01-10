2aaab097362368df85d2c653d2f09ce9fd6036d3
DIRECT0R-ctrl <DominantVortex@proton.me>
2026-01-11 00:28:10 +0100

    Make Session a singleton with getInstance method

--- core/Session.php
+++ core/Session.php
@@ -1,11 +1,19 @@
 <?php
 
 class Session {
+    private static $instance = null;
     
     public function __construct() {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
+    }
+    
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
     }
     
     public function set($key, $value) {
